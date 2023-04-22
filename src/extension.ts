61864810dfa763cf304371c235ea5c6feef7051b
Weida Hong <wdhongtw@gmail.com>
2023-04-23 02:31:03 +0800

    refactor: file name style and move implementation
    - user lower-case, single-word file name
    - move all implementations that depend VSCode API to extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,9 @@ import { tmpdir } from "os";
 import * as path from "path";
 
 import * as gpg from './indicator/gpg';
-import { VscodeOutputLogger } from './logger';
-import SecretObjectStorage from "./ObjectStorages/SecretObjectStorage";
-import MementoObjectStorage from "./ObjectStorages/MementoObjectStorage";
-import KeyStatusManager from "./KeyStatusManager";
+import { Logger } from "./indicator/logger";
+import KeyStatusManager from "./manager";
+import { AbstractObjectStorage, StorageObject } from "./manager";
 import { m } from "./message";
 
 type statusStyleEnum = "fingerprintWithUserId" | "fingerprint" | "userId";
@@ -315,4 +314,101 @@ export async function deactivate() {
         recursive: true,
         force: true,
     });
+}
+
+const timeStr = (date = new Date()) => date.toISOString();
+
+enum LogLevel {
+    error = 1,
+    warning,
+    info
+}
+
+export class VscodeOutputLogger implements Logger {
+    private outputChannel: vscode.OutputChannel;
+    private level: LogLevel;
+    /**
+     * @param name - The name of VS Code output channel on UI
+     * @param level - The log level for the logger
+     */
+    constructor(name: string, level: string) {
+        this.outputChannel = vscode.window.createOutputChannel(name);
+        this.level = VscodeOutputLogger.levelFromString(level);
+    }
+
+    static levelFromString(level: string): LogLevel {
+        switch (level) {
+            case "error":
+                return LogLevel.error;
+            case "warning":
+                return LogLevel.warning;
+            case "info":
+                return LogLevel.info;
+            default:
+                throw new Error(`unknown log level: ${level}`);
+        }
+    }
+
+    setLevel(level: string): void {
+        this.level = VscodeOutputLogger.levelFromString(level);
+    }
+
+    /**
+     * Log some message at info level.
+     * @param message - a message without ending new line
+     */
+    info(message: string): void {
+        if (this.level < LogLevel.info) {
+            return;
+        }
+        this.outputChannel.appendLine(`[${timeStr()}] [INFO] ` + message);
+    }
+
+    /**
+     * Log some message at warning level.
+     * @param message - a message without ending new line
+     */
+    warn(message: string): void {
+        if (this.level < LogLevel.warning) {
+            return;
+        }
+        this.outputChannel.appendLine(`[${timeStr()}] [WARN] ` + message);
+    }
+
+    /**
+     * Log some message at error level.
+     * @param message - a message without ending new line
+     */
+    error(message: string): void {
+        if (this.level < LogLevel.error) {
+            return;
+        }
+        this.outputChannel.appendLine(`[${timeStr()}] [ERROR] ` + message);
+    }
+}
+
+
+class MementoObjectStorage extends AbstractObjectStorage {
+    constructor(private storageUtility: vscode.Memento, protected logger: VscodeOutputLogger) {
+        super();
+    }
+    protected async _getAll(): Promise<StorageObject> {
+        return await this.storageUtility.get(this.key) || {};
+    }
+    protected async _setStorage(storage: StorageObject): Promise<void> {
+        await this.storageUtility.update(this.key, storage);
+    }
+}
+
+
+class SecretObjectStorage extends AbstractObjectStorage {
+    constructor(private storageUtility: vscode.SecretStorage, protected logger: VscodeOutputLogger) {
+        super();
+    }
+    protected async _getAll(): Promise<StorageObject> {
+        return JSON.parse(await this.storageUtility.get(this.key) || "{}");
+    }
+    protected async _setStorage(storage: StorageObject): Promise<void> {
+        await this.storageUtility.store(this.key, JSON.stringify(storage));
+    }
 }
--- src/manager.ts
+++ src/manager.ts
@@ -4,7 +4,6 @@ import * as git from './indicator/git';
 import * as gpg from './indicator/gpg';
 import * as process from './indicator/process';
 import type { Logger } from './indicator/logger';
-import type SecretObjectStorage from "./ObjectStorages/SecretObjectStorage";
 import locker from "./indicator/locker";
 import { m } from "./message";
 
@@ -36,7 +35,7 @@ export default class KeyStatusManager {
     constructor(
         private logger: Logger,
         private syncInterval: number,
-        private secretStorage: SecretObjectStorage,
+        private secretStorage: AbstractObjectStorage,
         public enableSecurelyPassphraseCache: boolean,
         private isWorkspaceTrusted: boolean,
         private tmp: string,
@@ -258,3 +257,65 @@ export default class KeyStatusManager {
         this.disposed = true;
     }
 }
+
+export type StorageValue = string | boolean | number;
+
+export type StorageObject = Record<string, StorageValue>;
+
+export abstract class AbstractObjectStorage {
+    protected key = "gpg-indicator-storage_" + this.constructor.name; // DON'T CHANGE IT AFTER V0.7
+
+    protected abstract logger: Logger;
+    protected abstract _getAll(): Promise<StorageObject>;
+    protected abstract _setStorage(storage: StorageObject): Promise<void>;
+
+    async getAll(): Promise<StorageObject> {
+        return await locker.acquire(this.key, async (release) => {
+            try {
+                release(undefined, await this._getAll());
+            } catch (e) {
+                release(e as any);
+            }
+        });
+    }
+    protected async setStorage(storage: StorageObject): Promise<void> {
+        await locker.acquire(this.key, async (release) => {
+            try {
+                await this._setStorage(storage);
+                release();
+            } catch (e) {
+                this.logger.error(`Cannot set the raw data to ${this.constructor.name}: ${e instanceof Error ? e.message : JSON.stringify(e, null, 4)}`);
+                release(e as any);
+            }
+        });
+    }
+    async get(key: string): Promise<StorageValue | undefined> {
+        return (await this.getAll())[key];
+    }
+    async set(key: string, value: StorageValue): Promise<void> {
+        const storage = await this.getAll();
+        storage[key] = value;
+        await this.setStorage(storage);
+    }
+    async has(key: string): Promise<boolean> {
+        return Reflect.has(await this.getAll(), key);
+    }
+    async delete(key: string): Promise<void> {
+        const storage = await this.getAll();
+        Reflect.deleteProperty(storage, key);
+        await this.setStorage(storage);
+    }
+    async clear(): Promise<void> {
+        await this.setStorage({});
+    }
+    async keys(): Promise<string[]> {
+        return Object.keys(await this.getAll());
+    }
+    async values(): Promise<StorageValue[]> {
+        return Object.values(await this.getAll());
+    }
+    async entries(): Promise<[string, StorageValue][]> {
+        return Object.entries(await this.getAll());
+    }
+}
+
